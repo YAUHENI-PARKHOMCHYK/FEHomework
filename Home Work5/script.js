1601393c8fd153e2d74413a6893203769b9301c1
YAUHENI-PARKHOMCHYK <118852850+YAUHENI-PARKHOMCHYK@users.noreply.github.com>
2023-02-25 18:49:08 +0100

    Add files via upload

---
+++ Home Work5/script.js
@@ -0,0 +1,25 @@
+let number = prompt("Введите число!");
+console.log(` ${"Квадрат вашего числа: "}` +number **2);
+
+
+
+
+let number1 = prompt("Введите число!");
+if(+number1 === 0){
+console.log("Ваше число равно нулю!")}
+if(+number1 % 2 === 0 && +number1 !== 0){
+console.log("Ваше число четное!")}
+if(+number1 % 2 === 1){
+    console.log("Ваше число не четное!")};
+
+
+
+
+
+let meaning = prompt("Введите значение!");
+
+if(meaning == +meaning){
+console.log("Ваше значение можно преобразовать в число!")
+}else{
+console.log("Ваше значение преобразовать в число не получится!")
+};
